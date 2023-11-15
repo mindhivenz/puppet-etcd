10fe7e1168e02612cec6b03eda050cebb649c4a4
damon <damon@mahuika.co.nz>
2023-11-16 09:38:01 +1300

    More tests

--- spec/classes/client_spec.rb
+++ spec/classes/client_spec.rb
@@ -4,8 +4,31 @@ require 'spec_helper'
 
 describe 'etcd::client' do
   on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
+    context "present on #{os}" do
       let(:facts) { os_facts }
+      let(:pre_condition) do
+        <<~EOS
+          class { etcd:
+            version => '3.5.0',
+          }
+          class { etcd::ca:
+            ca_cert_content   => 'some-cert',
+            ca_key_content    => 'some-key',
+            ca_key_passphrase => 'some-passphrase', 
+          }
+        EOS
+      end
+
+      it { is_expected.to compile.with_all_deps }
+    end
+
+    context "absent on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) do
+        {
+          'ensure' => 'absent',
+        }
+      end
       let(:pre_condition) do
         <<~EOS
           class { etcd:
--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -4,11 +4,35 @@ require 'spec_helper'
 
 describe 'etcd::server' do
   on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
+    context "present on #{os}" do
       let(:facts) { os_facts }
       let(:params) do
         {
           'token' => 'some-token',
+        }
+      end
+      let(:pre_condition) do
+        <<~EOS
+          class { etcd:
+            version => '3.5.0',
+          }
+          class { etcd::ca:
+            ca_cert_content   => 'some-cert',
+            ca_key_content    => 'some-key',
+            ca_key_passphrase => 'some-passphrase', 
+          }
+        EOS
+      end
+
+      it { is_expected.to compile.with_all_deps }
+    end
+
+    context "absent on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) do
+        {
+          'token' => 'some-token',
+          'ensure' => 'absent',
         }
       end
       let(:pre_condition) do
